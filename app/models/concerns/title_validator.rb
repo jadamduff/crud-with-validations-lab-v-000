4067fa65f018266b1770608ff51c73064da1a828
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 23:05:50 +0000

    automatically backed up by learn

--- app/models/concerns/title_validator.rb
+++ app/models/concerns/title_validator.rb
@@ -1,7 +1,7 @@
 class TitleValidator < ActiveModel::Validator
   def validate(record)
     if record[:artist_name]
-      if !Song.where("release_year = ? AND author_name = ? AND title = ?", record[:release_year], record[:author_name], record[:title]).empty?
+      if !Song.where("release_year = ? AND artist_name = ? AND title = ?", record[:release_year], record[:artist_name], record[:title]).empty?
         record.errors[:title] << 'song already exists.'
       end
     end

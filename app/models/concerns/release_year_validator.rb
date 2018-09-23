011d0a1728b38b94cc36cec15fa043b199dab5d6
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 23:27:23 +0000

    automatically backed up by learn

--- app/models/concerns/release_year_validator.rb
+++ app/models/concerns/release_year_validator.rb
@@ -2,6 +2,8 @@ class ReleaseYearValidator < ActiveModel::Validator
   def validate(record)
     if record[:release_year].to_i > Time.now.year
       record.errors[:release_year] << "This is in the future..."
+    elsif record[:released] == true && record[:release_year] == ''
+      record.errors[:release_year] << "Release Year required if song has been released."
     end
   end
 end

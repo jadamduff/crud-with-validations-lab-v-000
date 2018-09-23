a67897108b377be32c8adac3826206927b2a716b
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 23:11:21 +0000

    automatically backed up by learn

--- app/models/concerns/release_year_validator.rb
+++ app/models/concerns/release_year_validator.rb
@@ -1,6 +1,6 @@
 class ReleaseYearValidator < ActiveModel::Validator
   def validate(record)
-    if record[:release_year] > Time.now.year
+    if record[:release_year].to_i > Time.now.year
       record.errors[:release_year] << "This is in the future..."
     end
   end

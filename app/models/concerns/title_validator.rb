2c13200117e9b8912a6cc2a9d81f4a358d37c1a4
Adam Duff <j.adam.duff@gmail.com>
2018-09-23 23:51:27 +0000

    automatically backed up by learn

--- app/models/concerns/title_validator.rb
+++ app/models/concerns/title_validator.rb
@@ -4,6 +4,8 @@ class TitleValidator < ActiveModel::Validator
       if !Song.where("release_year = ? AND artist_name = ? AND title = ?", record[:release_year], record[:artist_name], record[:title]).empty?
         record.errors[:title] << 'song already exists.'
       end
+    elsif record[:title] == nil
+      record.errors[:title] << "A title is required."
     end
   end
 end

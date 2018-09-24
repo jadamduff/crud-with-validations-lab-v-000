90f353f47566d65581d20c049f41ec257f87e51a
Adam Duff <j.adam.duff@gmail.com>
2018-09-24 01:05:28 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
-  validates_with TitleValidator
 
+  validates_with ReleaseYearValidator
   validates :title, presence: true
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true

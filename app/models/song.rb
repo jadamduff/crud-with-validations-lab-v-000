d56e0c6cc94e93a0d0029c3f6bb78c107d86d97e
Adam Duff <j.adam.duff@gmail.com>
2018-09-24 01:21:58 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,7 @@
 class Song < ActiveRecord::Base
-  validates_with TitleValidator
+  validates :title, uniqueness: { scope: [:release_year, :artist_name]}
   validates_with ReleaseYearValidator
   validates :title, presence: true
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
-  validates :title, uniqueness: { scope: [:release_year, :artist_name]}
 end

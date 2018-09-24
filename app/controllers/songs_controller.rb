b266b2e10df26876450af7c0a784ccfb13b0dcf4
Adam Duff <j.adam.duff@gmail.com>
2018-09-24 00:14:05 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,7 +43,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.permit(:song, :title, :released, :release_year, :artist_name, :genre)
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 
   def set_song!
